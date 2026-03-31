663729a37c86cc3e298e1206a77d00a8060bea51
JereWL <jerewl@metropolia.fi>
2026-03-31 18:11:28 +0300

    Moduuli 2 valmiiksi

---
+++ Module_2/Teht_5/Tehtävä_5.js
@@ -0,0 +1,15 @@
+const numbers = [];
+
+for (let i = 0;; i++) {
+    let number = prompt("Enter a number:");
+    if (numbers.includes(Number(number))) {
+        alert("Number already entered. Stopping input.");
+        break;
+    }
+    numbers.push(Number(number));
+}
+
+numbers.sort(function(a, b) {
+    return a - b;
+});
+console.log("Numbers in the list:", numbers);
---
+++ Module_2/Teht_8/Tehtävä_8.js
@@ -0,0 +1,10 @@
+function concat(array1) {
+    let result = "";
+    for (let i = 0; i < array1.length; i++) {
+        result += array1[i];
+    }
+    return result;
+}
+
+const array1 = ["Jere", "on", "kiva", "poika"];
+document.getElementById("target").textContent = concat(array1);
